728b1f31ed1cb3814f117aadd3599909eb278369
Ashraful Alam <ashrafcse3@gmail.com>
2022-10-25 21:30:58 +0100

    fetch data to left sidebar

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 const LeftSideNav = () => {
+    const [titles, setTitles] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/courses-title')
+            .then(res => res.json())
+            .then(data => setTitles(data))
+    }, [])
+
     return (
         <div>
             from leftsidenav.js
+            <br></br>
+            {titles.length}
         </div>
     );
 };
