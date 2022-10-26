b2f32f426c0faf5e41ebc1914ffc6e6e97536c82
Ashraful Alam <ashrafcse3@gmail.com>
2022-10-26 14:41:34 +0100

    login and logout change dynamically

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -7,61 +7,76 @@ import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 import './Header.css';
 
 const Header = () => {
-    const { user } = useContext(AuthContext);
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => {
+                console.error(error);
+            })
+    };
 
     return (
-        <div>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar bg="light" variant="light">
-                        <Container>
-                            <Link to='/' className='navbar-brand'>
-                                <img
-                                    alt=""
-                                    src="https://cdn-icons-png.flaticon.com/512/760/760921.png"
-                                    width="30"
-                                    height="30"
-                                    className="d-inline-block align-top"
-                                />{' '}
-                                English Gugu
-                            </Link>
-                        </Container>
-                    </Navbar>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Link to='/' className='nav-link'>Courses</Link>
-                            <Link to='/faq' className='nav-link'>FAQ</Link>
-                            <Link to='/blog' className='nav-link'>Blog</Link>
-                        </Nav>
-                        <Nav className="d-flex">
-                            {/* Choosing theme start */}
-                            <NavDropdown title="Theme" id="basic-nav-dropdown" className='me-2'>
-                                <NavDropdown.Item href="#action/3.1">Light</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.1">Dark</NavDropdown.Item>
-                            </NavDropdown>
-                            {/* Choosing theme end */}
-                            <Button className="me-2" variant="outline-success"><Link to='/login' className='nav-link'>Login</Link></Button>
-                            <Button className="me-2" variant="outline-success"><Link to='/signup' className='nav-link'>Sign up</Link></Button>
-                            <OverlayTrigger
-                                key='bottom'
-                                placement='bottom'
-                                overlay={
-                                    <Tooltip id='bottom'>
-                                        <strong>{user?.displayName}</strong>
-                                    </Tooltip>
-                                }>
-                                <Button className="me-2" variant="outline-success">
-                                    {
-                                        user ? <Image style={{ height: '30px' }} src={user.photoURL}></Image> : <FaUserCircle />
-                                    }
-                                </Button>
-                            </OverlayTrigger>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar bg="light" variant="light">
+                    <Container>
+                        <Link to='/' className='navbar-brand'>
+                            <Image
+                                alt=""
+                                src="https://cdn-icons-png.flaticon.com/512/760/760921.png"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />{' '}
+                            English Gugu
+                        </Link>
+                    </Container>
+                </Navbar>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link to='/' className='nav-link'>Courses</Link>
+                        <Link to='/faq' className='nav-link'>FAQ</Link>
+                        <Link to='/blog' className='nav-link'>Blog</Link>
+                    </Nav>
+                    <Nav className="d-flex">
+                        {/* Choosing theme start */}
+                        <NavDropdown title="Theme" id="basic-nav-dropdown" className='me-2'>
+                            <NavDropdown.Item href="#action/3.1">Light</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1">Dark</NavDropdown.Item>
+                        </NavDropdown>
+                        {/* Choosing theme end */}
+                        {
+                            !user?.uid ?
+                                <>
+                                    <Button className="me-2" variant="outline-success"><Link to='/login' className='nav-link'>Login</Link></Button>
+                                    <Button className="me-2" variant="outline-success"><Link to='/signup' className='nav-link'>Sign up</Link></Button>
+                                </>
+                                :
+                                <>
+                                    <OverlayTrigger
+                                        key='bottom'
+                                        placement='bottom'
+                                        overlay={
+                                            <Tooltip id='bottom'>
+                                                {user?.displayName}
+                                            </Tooltip>
+                                        }>
+                                        <Button className="me-2" variant="outline-success">
+                                            {
+                                                user ? <Image style={{ height: '30px' }} src={user?.photoURL}></Image> : <FaUserCircle />
+                                            }
+                                        </Button>
+                                    </OverlayTrigger>
+                                    <Button onClick={handleLogOut} className="me-2" variant="outline-success">Logout</Button>
+                                </>
+                        }
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
